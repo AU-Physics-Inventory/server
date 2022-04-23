274929dded6edc5e961065a8efa87a13bd93eb07
Nathanael Gutierrez <nathanaelg16@gmail.com>
2022-04-23 16:54:01 -0700

    can get claims directly from token object

--- src/main/kotlin/edu/andrews/cas/physics/inventory/server/auth/AuthorizationToken.java
+++ src/main/kotlin/edu/andrews/cas/physics/inventory/server/auth/AuthorizationToken.java
@@ -1,3 +1,17 @@
 package edu.andrews.cas.physics.inventory.server.auth;
 
-public record AuthorizationToken(String token) {}
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+
+import javax.crypto.SecretKey;
+
+public record AuthorizationToken(String token) {
+    public Jws<Claims> getClaims(SecretKey secretKey) {
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(this.token);
+    }
+}
