58415706992610130759d0a462818b1bc55248b1
Nathanael Gutierrez <nathanaelg16@gmail.com>
2022-04-23 16:46:45 -0700

    changes to endpoints

--- src/main/kotlin/edu/andrews/cas/physics/inventory/server/controller/AdminController.kt
+++ src/main/kotlin/edu/andrews/cas/physics/inventory/server/controller/AdminController.kt
@@ -3,6 +3,7 @@ package edu.andrews.cas.physics.inventory.server.controller
 import edu.andrews.cas.physics.inventory.server.exception.AlreadyRegisteredException
 import edu.andrews.cas.physics.inventory.server.model.User
 import edu.andrews.cas.physics.inventory.server.model.UserStatus
+import edu.andrews.cas.physics.inventory.server.request.ChangeUserStatusRequest
 import edu.andrews.cas.physics.inventory.server.request.UserInvitation
 import edu.andrews.cas.physics.inventory.server.request.UserRole
 import edu.andrews.cas.physics.inventory.server.service.AdminService
@@ -22,7 +23,7 @@ import org.springframework.web.bind.annotation.RequestParam
 @Controller
 class AdminController @Autowired constructor(private val adminService: AdminService, private val authService: AuthenticationService) {
 
-    @PostMapping("/admin/inviteUser")
+    @PostMapping("/admin/user/invite")
     fun inviteUserToRegister(@RequestBody userInvitation: UserInvitation) : ResponseEntity<Any> {
         logger.info("[Admin Controller] Received request to invite user with email address {} to register", userInvitation.email)
         return try {
@@ -40,38 +41,38 @@ class AdminController @Autowired constructor(private val adminService: AdminServ
         }
     }
 
-    @PostMapping("/admin/addUserRole")
+    @PostMapping("/admin/user/addRole")
     fun addUserRole(@RequestBody userRole: UserRole) : ResponseEntity<Any> {
         logger.info("[Admin Controller] Received request to add role '{}' to user '{}'", userRole.role, userRole.username)
         adminService.addUserRole(userRole)
         return ResponseEntity.accepted().build()
     }
 
-    @PostMapping("/admin/removeUserRole")
+    @PostMapping("/admin/user/removeRole")
     fun removeUserRole(@RequestBody userRole: UserRole) : ResponseEntity<Any> {
         logger.info("[Admin Controller] Received request to remove role '{}' from user '{}'", userRole.role, userRole.username)
         adminService.removeUserRole(userRole)
         return ResponseEntity.accepted().build()
     }
 
-    @GetMapping("/admin/logoutUser")
-    fun logoutUser(@RequestParam user: String) : ResponseEntity<Any> {
+    @PostMapping("/admin/user/logout")
+    fun logoutUser(@RequestBody user: String) : ResponseEntity<Any> {
         return if (authService.logout(user)) ResponseEntity.ok().build()
         else ResponseEntity.internalServerError().build()
     }
 
-    @GetMapping("/admin/changeUserStatus")
-    fun changeUserStatus(@RequestParam user: String, @RequestParam status: UserStatus) : ResponseEntity<Any> {
-        logger.info("[Admin Controller] Received request to set status for user '{}' as '{}'", user, status)
-        if (status == UserStatus.LOCKED || status == UserStatus.PENDING) return ResponseEntity.badRequest().body("Administrators are only able to activate or deactivate users.")
-        adminService.changeUserStatus(user, status)
+    @PostMapping("/admin/user/changeStatus")
+    fun changeUserStatus(@RequestBody request: ChangeUserStatusRequest) : ResponseEntity<Any> {
+        logger.info("[Admin Controller] Received request to set status for user '{}' as '{}'", request.username, request.status)
+        if (request.status == UserStatus.LOCKED || request.status == UserStatus.PENDING) return ResponseEntity.badRequest().body("Administrators are only able to activate or deactivate users.")
+        adminService.changeUserStatus(request)
         return ResponseEntity.accepted().build()
     }
 
     @GetMapping("/admin/users")
-    fun getUsers() : ResponseEntity<List<User>> {
-        logger.info("[Admin Controller] Received request to retrieve all user documents")
-        return ResponseEntity.ok(adminService.getUsers())
+    fun getUsers(@RequestParam("withRole", required = false) role: String?) : ResponseEntity<List<User>> {
+        logger.info("[Admin Controller] Received request to retrieve all user documents {}", if (role != null && role.isNotEmpty()) String.format("with role '%s'", role) else "")
+        return ResponseEntity.ok(adminService.getUsers(role))
     }
 
     companion object {
