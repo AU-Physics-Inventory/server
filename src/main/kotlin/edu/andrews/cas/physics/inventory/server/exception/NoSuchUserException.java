dd6b263bbe886eacd434cf828325c0af1ceb0261
Nathanael Gutierrez <nathanaelg16@gmail.com>
2022-04-23 17:00:16 -0700

    code refactoring

--- src/main/kotlin/edu/andrews/cas/physics/inventory/server/exception/NoSuchUserException.java
+++
@@ -1,7 +0,0 @@
-package edu.andrews.cas.physics.inventory.server.exception;
-
-public class NoSuchUserException extends RuntimeException {
-    public NoSuchUserException(String message) {
-        super(message);
-    }
-}
---
+++ src/main/kotlin/edu/andrews/cas/physics/inventory/server/exception/NoSuchUserException.kt
@@ -0,0 +1,3 @@
+package edu.andrews.cas.physics.inventory.server.exception
+
+class NoSuchUserException constructor(msg: String) : RuntimeException(msg)
