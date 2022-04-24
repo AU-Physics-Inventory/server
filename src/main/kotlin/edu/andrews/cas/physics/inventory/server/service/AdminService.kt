08c72234bdbc9d2c24b0314c93b6fc91c13a8a1f
Nathanael Gutierrez <nathanaelg16@gmail.com>
2022-04-23 20:15:45 -0700

    access codes are now uppercase

--- src/main/kotlin/edu/andrews/cas/physics/inventory/server/service/AdminService.kt
+++ src/main/kotlin/edu/andrews/cas/physics/inventory/server/service/AdminService.kt
@@ -11,6 +11,7 @@ import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class AdminService @Autowired constructor(private val adminDAO: AdminDAO, private val emailService: EmailService) {
@@ -32,7 +33,7 @@ class AdminService @Autowired constructor(private val adminDAO: AdminDAO, privat
     }
 
     private fun generateAccessCode(): String {
-        return RandomStringUtils.randomAlphanumeric(6)
+        return RandomStringUtils.randomAlphanumeric(6).uppercase(Locale.getDefault())
     }
 
     fun addUserRole(userRole: UserRole) {
