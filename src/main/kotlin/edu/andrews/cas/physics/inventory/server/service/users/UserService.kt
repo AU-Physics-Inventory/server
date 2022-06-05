cfb937b8d5a64c5312efd81487a058869c8b3bb1
Nathanael Gutierrez <nathanaelg16@gmail.com>
2022-06-04 18:55:37 -0700

    spacing

--- src/main/kotlin/edu/andrews/cas/physics/inventory/server/service/users/UserService.kt
+++ src/main/kotlin/edu/andrews/cas/physics/inventory/server/service/users/UserService.kt
@@ -1,8 +1,10 @@
-package edu.andrews.cas.physics.inventory.server.service
+package edu.andrews.cas.physics.inventory.server.service.users
 
 import edu.andrews.cas.physics.inventory.server.auth.AuthorizationToken
 import edu.andrews.cas.physics.inventory.server.dao.UserDAO
 import edu.andrews.cas.physics.inventory.server.request.user.UserRegistration
+import edu.andrews.cas.physics.inventory.server.service.authentication.AuthenticationService
+import edu.andrews.cas.physics.inventory.server.service.authentication.EmailService
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 import org.springframework.beans.factory.annotation.Autowired
@@ -10,10 +12,12 @@ import org.springframework.stereotype.Service
 import javax.crypto.SecretKey
 
 @Service
-class UserService @Autowired constructor(private val secretKey: SecretKey,
-                                         private val userDAO: UserDAO,
-                                         private val emailService: EmailService,
-                                         private val authenticationService: AuthenticationService) {
+class UserService @Autowired constructor(
+    private val secretKey: SecretKey,
+    private val userDAO: UserDAO,
+    private val emailService: EmailService,
+    private val authenticationService: AuthenticationService
+) {
     fun changeEmail(jwt: AuthorizationToken, email: String) {
         val claims = jwt.getClaims(secretKey)
         val user = claims.body.subject
